d34d3ec573d08521901ff3c662e318875cb6542f
Uladzimir Makarau <uladzimir.makarau@gmail.com>
2019-06-23 14:08:16 +0300

    Initial

---
+++ FrotoTpUsage/Program.fs
@@ -0,0 +1,29 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open Froto.TypeProvider
+open Froto.Serialization
+
+type Books = ProtocolBuffersTypeProvider<"books.proto">
+
+
+[<EntryPoint>]
+let main argv =
+    let b = new Books.BookAuthor()
+    b.Name <- Some "foo"
+
+    let bb = new Books.BookDetails()
+    bb.Author <- Some b
+
+    printfn "Total size - %i" bb.SerializedLength
+
+    let zcb = new ZeroCopyBuffer(100)
+
+    bb.Serialize(zcb);
+    let buffer' = new ZeroCopyBuffer(zcb.AsArraySegment)
+
+    let bb' = Books.BookDetails.Deserialize buffer'
+
+    printf "Author name - %A" (bb'.Author |> Option.map(fun a -> a.Name))
+
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
